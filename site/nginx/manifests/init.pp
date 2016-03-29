132752f54bef249ee72fe5a689c9895ed5d7a9fd
azimmerman0125 <azimmerman@imo-online.com>
2016-03-29 14:02:10 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,6 +3,42 @@ class nginx {
     ensure => present,
   }
   
+  file {'/etc':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
   
+    file {'/etc/nginx':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }  
+  
+  file {'/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    souce => 'puppet:///modules/ngix/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
 
+  file {'/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    souce => 'puppet:///modules/ngix/index.html',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+
+  service {'nginx': 
+    ensure => running,
+    enable => true,
+  }
 }
