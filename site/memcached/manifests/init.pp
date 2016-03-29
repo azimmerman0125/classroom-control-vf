d6f18409d67f908d7195d4cb0c3d7319376b5f56
azimmerman0125 <azimmerman@imo-online.com>
2016-03-29 12:48:40 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,3 +1,16 @@
 class memcached {
-
+  package {'memcached': 
+    ensure => present,
+  }
+  
+  file {'/etc/sysconfig/memcached':
+    ensure => file,
+    requires => Package ['memcached']
+  }
+  
+  service {'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File ['/etc/sysconfig/memcached']
+  }
 }
