fa1f528856d0c758d1804e5ef102b461d79955d3
azimmerman0125 <azimmerman@imo-online.com>
2016-03-30 13:23:36 -0500

    Update managed_users.pp

--- site/users/manifests/managed_users.pp
+++ site/users/manifests/managed_users.pp
@@ -1,15 +1,23 @@
-File {
 
 define users::managed_user (
-  $user = $title,
+  $user,
   $shell = '/bin/bash',
   $home = "/home/${user}",
+  $group = $user,
   ) {
+  $user = $title
+  File  {
+    owner => $user,
+    group => $group,
+  }
   user { $user:
     ensure => present,
     shell => $shell,
     home => $home,
+    group => $user,
   }
-file { "${home}/.ssh":
-  ensure => directory
+  file { "${home}/.ssh":
+    ensure => directory,
+    mode => '0750',
+  }
 }
