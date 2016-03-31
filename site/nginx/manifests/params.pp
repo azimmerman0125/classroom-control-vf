154a739c7b81e0bd6bf87bec6f0b288bf36f7eda
azimmerman01 <root@azimmerman0125.puppetlabs.vm>
2016-03-31 17:20:31 +0000

    New Updates

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -4,6 +4,7 @@ class nginx::params {
       $nginx_package_name = 'nginx'
       $nginx_owner = 'root'
       $nginx_group = 'root'
+      $nginx_document_root = '/var/www'
       $nginx_home_dir = '/etc/nginx'
       $nginx_config_dir = $nginx_home_dir
       $nginx_pid_dir = '/var/run'
@@ -14,6 +15,7 @@ class nginx::params {
       $nginx_package_name = 'nginx'
       $nginx_owner = 'root'
       $nginx_group = 'root'
+      $nginx_document_root = '/var/www'
       $nginx_home_dir = '/etc/nginx'
       $nginx_config_dir = $nginx_home_dir
       $nginx_pid_dir = '/var/run'
@@ -26,6 +28,7 @@ class nginx::params {
       $nginx_owner = 'Administrator'
       $nginx_group = 'Administrators'
       $nginx_home_dir = 'C:/ProgramData/nginx'
+      $nginx_document_root = "${nginx_home_dir}/html"
       $nginx_config_dir = $nginx_home_dir
       $nginx_pid_dir = $nginx_home_dir
       $nginx_log_dir = "${nginx_home_dir}/logs"
