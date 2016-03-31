e6e7020de7dd39e0fa306c04900b917dfa014b66
azimmerman01 <root@azimmerman0125.puppetlabs.vm>
2016-03-31 14:49:53 +0000

    New Updates

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,28 +1,31 @@
 #Define class nginx
-class nginx {
+class nginx (
+  $root = undef,
+) {
   case $::osfamily {
     'debian': {
       $nginx_package_name = 'nginx'
       $nginx_owner = 'root'
       $nginx_group = 'root'
-      $nginx_document_root = '/var/www'
+      #$nginx_document_root = '/var/www'
       $nginx_home_dir = '/etc/nginx'
       $nginx_config_dir = $nginx_home_dir
       $nginx_pid_dir = '/var/run'
       $nginx_log_dir = '/var/log/nginx'
       $nginx_service_runasuser = 'nginx'
-
+      $default_root = "/var/www"
     }
     'redhat': {
       $nginx_package_name = 'nginx'
       $nginx_owner = 'root'
       $nginx_group = 'root'
-      $nginx_document_root = '/var/www'
+      #$nginx_document_root = '/var/www'
       $nginx_home_dir = '/etc/nginx'
       $nginx_config_dir = $nginx_home_dir
       $nginx_pid_dir = '/var/run'
       $nginx_log_dir = '/var/log/nginx'
       $nginx_service_runasuser = 'www-data'
+      $default_root = "/var/www"
     }
 
     'windows': {
@@ -30,12 +33,12 @@ class nginx {
       $nginx_owner = 'Administrator'
       $nginx_group = 'Administrators'
       $nginx_home_dir = 'C:/ProgramData/nginx'
-      $nginx_document_root = "${nginx_home_dir}/html"
+      #$nginx_document_root = "${nginx_home_dir}/html"
       $nginx_config_dir = $nginx_home_dir
       $nginx_pid_dir = $nginx_home_dir
       $nginx_log_dir = "${nginx_home_dir}/logs"
       $nginx_service_runasuser = 'nobody'
-
+      $default_root = "C:/ProgramData/nginx/html"
     }
 
     default: {
@@ -47,6 +50,12 @@ class nginx {
   #Setting the default values for all OSes
   $nginx_serverblock_dir = "${nginx_home_dir}/conf.d"
   $nginx_service_name = 'nginx'
+
+
+  $nginx_document_root = $nginx_document_root ? {
+    undef => $default_root,
+    default => $root,
+  }
 
   File {
     ensure => 'file',
